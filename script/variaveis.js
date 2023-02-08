3c46ea1813c8a5eaa1540d0c1c83585c9a745049
saywako <124358844+saywako@users.noreply.github.com>
2023-02-08 11:46:30 -0300

    Add files via upload

---
+++ script/variaveis.js
@@ -0,0 +1,12 @@
+//tem duas formas de declararuma variavel
+//var e let
+/*let hello = 'ola turma'
+let mensagem;
+
+mensagem=hello
+
+alert(hello);
+alert(mensagem)*/
+
+const carro = ["fiat","golf", "bmw", "ferrary"]
+alert(carro);
